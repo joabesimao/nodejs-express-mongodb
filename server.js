d2014886e07d50118a02beb694dfdc88e957ec41
joabesimao <simaojoabecosta@gmail.com>
2022-04-15 20:26:12 -0300

    importa app

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-const { status } = require("express/lib/response");
-const res = require("express/lib/response");
-const http = require("http");
-const port = 3000;
+import app from "./src/app.js";
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("curso de node");
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`servidor escutando em http://localhost:${port}`);
 });
