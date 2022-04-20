03b164d05f6959289f4c3a3c0add8f058d90ff9f
joabesimao <simaojoabecosta@gmail.com>
2022-04-20 19:27:16 -0300

    criar as rotas usando o controller

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,6 +3,10 @@ import LivroController from "../controllers/livrosController.js";
 
 const router = express.Router();
 
-router.get("/livros", LivroController.listrarLivros);
+router
+  .get("/livros", LivroController.listrarLivros)
+  .get("/livros/:id", LivroController.listarLivroPorId)
+  .post("/livros", LivroController.cadastrarLivro)
+  .put("/livros/:id", LivroController.atualizarLivro);
 
 export default router;
