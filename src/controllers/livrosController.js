9701344241ad34e7b7da0eb1ef081d42d5c6b26b
joabesimao <simaojoabecosta@gmail.com>
2022-04-20 19:26:46 -0300

    criar o controller

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -6,6 +6,46 @@ class LivroController {
       res.status(200).json(livros);
     });
   };
+
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id, (erro, livros) => {
+      if (erro) {
+        res
+          .status(400)
+          .send({ message: `${erro.message} id do livro não localizado` });
+      } else {
+        res.status(200).send(livros);
+      }
+    });
+  };
+
+  static cadastrarLivro = (req, res) => {
+    let livro = new livros(req.body);
+
+    livro.save((erro) => {
+      if (erro) {
+        res
+          .status(500)
+          .send({ message: `${erro.message} falha ao cadastrar o livro.` });
+      } else {
+        res.status(201).send(livro.toJSON());
+      }
+    });
+  };
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, { $set: req.body }, (erro) => {
+      if (!erro) {
+        res.status(200).send({ message: "livro atualizado com sucesso" });
+      } else {
+        res.status(500).send({ message: erro.message });
+      }
+    });
+  };
 }
 
 export default LivroController;
