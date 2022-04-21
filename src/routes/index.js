2c897a47fed775a69d90eb704639df8648867c9b
joabesimao <simaojoabecosta@gmail.com>
2022-04-21 15:54:50 -0300

    inclui o autores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import livros from "./livrosRoutes.js";
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send({ titulo: "curso de node" });
   });
 
-  app.use(express.json(), livros);
+  app.use(express.json(), livros, autores);
 };
 
 export default routes;
