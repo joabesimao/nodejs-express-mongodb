431a1ff7f4bab132e6dd2176aa8ca156919af6c4
joabesimao <simaojoabecosta@gmail.com>
2022-04-21 15:55:39 -0300

    cria as rotas usando livroController

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,9 +4,10 @@ import LivroController from "../controllers/livrosController.js";
 const router = express.Router();
 
 router
-  .get("/livros", LivroController.listrarLivros)
+  .get("/livros", LivroController.listarLivros)
   .get("/livros/:id", LivroController.listarLivroPorId)
   .post("/livros", LivroController.cadastrarLivro)
-  .put("/livros/:id", LivroController.atualizarLivro);
+  .put("/livros/:id", LivroController.atualizarLivro)
+  .delete("/livros/:id", LivroController.excluirLivro);
 
 export default router;
