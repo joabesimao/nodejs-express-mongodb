91134f6626e805025b06823854865ea9b82bdfaf
joabesimao <simaojoabecosta@gmail.com>
2022-04-24 12:21:34 -0300

    criar ref:autores

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema({
   id: { type: String },
   titulo: { type: String, required: true },
-  autor: { type: String, required: true },
+  autor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "autores",
+    required: true,
+  },
   editora: { type: String, required: true },
   numeroPaginas: { type: Number },
 });
